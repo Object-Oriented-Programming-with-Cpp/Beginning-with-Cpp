9af16562bd8bee4854996231a9fd92789bf4ba89
SivaSampath <76162764+SivaSampath@users.noreply.github.com>
2020-12-19 19:00:46 +0530

    Add files via upload

---
+++ Sum-of-two-numbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number1,number2,Sum;
+    cout<<"Enter two numbers\n";
+    cin>>number1;
+    cin>>number2;
+    Sum=number1+number2;
+    cout<<"Sum of two numbers="<<Sum<<"\n";
+    return 0;
+}
